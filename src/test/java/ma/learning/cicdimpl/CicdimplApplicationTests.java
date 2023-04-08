d6e7e774770bb359496773ab0068dc7cbee0514a
aliloumehdi <mehdialilou99@gmail.com>
2023-04-08 22:31:43 +0000

    init

---
+++ src/test/java/ma/learning/cicdimpl/CicdimplApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.learning.cicdimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdimplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
